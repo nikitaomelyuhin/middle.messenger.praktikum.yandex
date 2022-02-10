a91a9ab3a223ab7bf82a4b77952ab0daa1067347
Nikita Omelyukhin <nikita.omelyuhin@gmail.com>
2022-02-10 23:12:30 +0300

    fixed server settings

--- server.js
+++ server.js
@@ -1,24 +1,8 @@
 const express = require('express');
-// const handlebars = require('express-handlebars');
 const app = express();
 const PORT = 3000;
 
-
-// app.engine(
-//   'handlebars',
-//   handlebars({
-//     layoutsDir: 'static',
-//     defaultLayout: 'layout',
-//     extname: 'hbs',
-//   })
-// )
-
-// app.set("view engine", "hbs");
-
-
-app.use(express.static(__dirname + "/dist"), (req, res) => {
-  res.render('index.hbs')
-});
+app.use(express.static(__dirname + "/dist"))
 
 app.listen(PORT, function () {
   console.log(`Listening on port ${PORT}!`);
