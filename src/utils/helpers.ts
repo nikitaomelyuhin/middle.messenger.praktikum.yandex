d44acb8c6ec6c7f13942210fdb45ee3ed1fb9004
Nikita Omelyuhin <nikita.oemlyuhin@gmail.com>
2022-04-27 06:55:34 +0300

    added demo date convert

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -88,3 +88,26 @@ export function getQueryParameterByName(name: string, url = window.location.href
   if (!results[2]) return "";
   return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
+
+export function formatDate(date: string) {
+  interface DateTimeFormatOptions {
+    localeMatcher?: "lookup" | "best fit";
+    weekday?: "long" | "short" | "narrow";
+    era?: "long" | "short" | "narrow";
+    year?: "numeric" | "2-digit";
+    month?: "numeric" | "2-digit" | "long" | "short" | "narrow";
+    day?: "numeric" | "2-digit";
+    hour?: "numeric" | "2-digit";
+    minute?: "numeric" | "2-digit";
+    second?: "numeric" | "2-digit";
+    timeZoneName?: "long" | "short";
+    formatMatcher?: "basic" | "best fit";
+    hour12?: boolean;
+    timeZone?: string; // this is more complicated than the others, not sure what I expect here
+  }
+  const options: DateTimeFormatOptions = {
+    month: "short", day: "numeric", hour: "numeric", minute: "numeric", timeZone: "Europe/Moscow",
+  };
+  const supportDate: Date = new Date(date);
+  return supportDate.toLocaleDateString("ru-RU", options);
+}
